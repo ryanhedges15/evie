7704da299dd81a958e5c82614886788c4c79d32a
shannonhedges <shannonhedges@gmail.com>
2017-08-08 22:16:19 -0400

    Developed a scheduled service that queries collection stats and saves them to the database.

--- evie-application/src/main/java/com/evie/service/scheduled/CollectionStatsSchedulerService.java
+++ evie-application/src/main/java/com/evie/service/scheduled/CollectionStatsSchedulerService.java
@@ -1,7 +1,9 @@
 package com.evie.service.scheduled;
 
+import com.evie.service.CollectionStatsCommandService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -14,15 +16,22 @@ public class CollectionStatsSchedulerService {
 
     private static Logger log = LoggerFactory.getLogger(CollectionStatsSchedulerService.class);
 
+    @Autowired
+    private CollectionStatsCommandService collectionStatsCommandService;
+
 
     /**
      * Execute the annotated method with a fixed period in milliseconds between
      * invocations to log collection stats.
      */
     @Scheduled(fixedDelay = 1000L)
-    public void saveCollectionStats() {
-        log.info("Putting CollectionStats");
+    public void saveCollectionStats(){
+        log.info("Pulling CollectionStats");
+        collectionStatsCommandService.pullCurrentCollectionStats();
+        log.info("Finished pulling CollectionStats");
     }
+
+
 
 
 }
